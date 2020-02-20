5a85fd86effd4df5533e7f622d2c2f308387d186
JovanXin <61258436+JovanXin@users.noreply.github.com>
2020-02-20 15:22:00 +1300

    Removed need to use global variables
    + Changed some variable names and added comments for readability
    - Removed global variables

--- userpassgenerator.py
+++ userpassgenerator.py
@@ -3,35 +3,33 @@ import requests
 import random
 
 #a list of words
-WORD_SITE = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
-response = requests.get(WORD_SITE)
-possiblePasswords = response.content.splitlines()
+wordsite = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+response = requests.get(wordsite)
+wordlist = response.content.splitlines()
 
-def generator():
-  global username
+def generate_username():
   date = datetime.datetime.now() #getting current time
   year = (date.year) #getting the year
-  first_name = input("please type your first name : ")
+  fname = input("please type your first name : ")
 
-  last_name = input("please type your last name : ")
+  lname = input("please type your last name : ")
 
   age = (int(input("please type your age : ")) - 13) 
-  year = str(year - age)
-  username = (str(year[-1]) + first_name[0:1] + last_name[0:6]) #generates the username
-
-  print("Thank you, your username is " + username)
-
+  syear = str(year - age) #schoolyear
+  username = (str(syear[-1]) + fname[0:1] + lname[0:6]) #generates the username
   return username
 
 
-#generator()
-
-def passgenerator():
-  global password
+def generate_password(wordlist):
   # pick a random password
-  word = possiblePasswords[random.randint(0, len(possiblePasswords))] #gets the word
-  print(word)
-  number = random.randint(10,100) #gets the number
-  password = (str(number) + str(word))
-  print(password)
-passgenerator()
+  password = random.choice(wordlist)
+  return f"{password}{random.randint(10,100)}"
+
+def main():
+  wordlist = response.content.splitlines()
+  username = generate_username()
+  password = generate_password(wordlist)
+  print("Your password is" + password + "and your username is " + username)
+  
+if __name__ == "__main__":
+  main()
